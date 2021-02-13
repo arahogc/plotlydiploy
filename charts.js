d2db722c07ffbc0efb02fefedba6f7d176f45ab3
arahogc <74072742+arahogc@users.noreply.github.com>
2021-02-13 14:57:12 -0800

    updated javascript code to make bar chart, still have an error or two

--- charts.js
+++ charts.js
@@ -53,35 +53,56 @@ function buildMetadata(sample) {
   });
 }
 
-// 1. Create the buildCharts function.
+//1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-
+    var samples = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    var sampleArray = samples.filter(sampleObj => sampleObj.id == sample);
     //  5. Create a variable that holds the first sample in the array.
+    var result = sampleArray[0];
+    console.log(result); 
 
+    //6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+    var otu_ids = result.otu_ids;
+    var otu_labels = result.otu_labels;
+    var sample_values = result.sample_values;
+    console.log(otu_ids, otu_labels, sample_values); 
 
-    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+    //7. Create the yticks for the bar chart.
+    //Hint: Get the the top 10 otu_ids and map them in descending order  
+    // so the otu_ids with the most bacteria are last. 
 
+    var sorted_sample_values = sampleArray.sort((a,b) => b.sample_values - a.sample_values).reverse();
+    console.log(sorted_sample_values); 
 
-    // 7. Create the yticks for the bar chart.
-    // Hint: Get the the top 10 otu_ids and map them in descending order  
-    //  so the otu_ids with the most bacteria are last. 
+    var sorted_sample_slice = sample_values.slice(0, 10);
+    var sorted_otu_ids = otu_ids.slice(0,10);
+    var sorted_otu_labels = otu_labels.slice(0,10);
+    console.log(sorted_sample_slice, sorted_otu_ids, sorted_otu_labels); 
 
-    var yticks = 
+    var yticks = sorted_otu_ids.map(id => "OTU: " + id);
+    var yticks = sorted_sample_slice.map(sorted_sample_slice => sorted_sample_slice.sample_values);
+    console.log(yticks, sorted_sample_slice); 
 
-    // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
-    // 9. Create the layout for the bar chart. 
-    var barLayout = {
-     
+    ///8. Create the trace for the bar chart. 
+    var trace = {
+      x: sorted_sample_values,
+      y: yticks,
+      type: "bar",
+      orientation: 'h',
+      text: sorted_otu_labels // this creates the hovertext
     };
-    // 10. Use Plotly to plot the data with the layout. 
-    
+    var barData = [trace];
+   
+    //9. Create the layout for the bar chart. 
+    var barLayout = {
+      title: "Top 10 Bacteria Cultures Found",
+      yaxis: {autorange: 'reversed'} // this reverses the yaxis
+    };
+    //10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot("bar", barData, barLayout);
   });
-}
+};
